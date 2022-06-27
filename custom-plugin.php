dfaa545a922f8296bcce8b38be03515d47f75e57
Leigh-Anne Bowers <79365679+LeighBowers@users.noreply.github.com>
2022-06-27 13:51:08 +0200

    Add files via upload

---
+++ custom-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name:       Custom Plugin
+ * Description:       Carousel plugin for wordpress
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Team XL
+ */
